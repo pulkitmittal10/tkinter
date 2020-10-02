55ade86168f6d72e67b285e6293ee69e23540120
Pulkit Mittal <pulkitmittal0502@gmail.com>
2020-10-02 18:30:40 +0100

    now a user can lookup weather information on a zipcode of their choice!

--- weather.py
+++ weather.py
@@ -5,33 +5,45 @@ import json
 
 root = Tk()
 root.title('Weather')
-# root.geometry('300x30')
+root.geometry('500x85')
 
-try:
-	api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode=89129&distance=5&API_KEY=0E466704-C530-4042-A2B3-EFA2C002C829")
-	api = json.loads(api_request.content)
-	city = api[0]['ReportingArea']
-	quality = api[0]['AQI']
-	category = api[0]['Category']['Name']
+def zipLookup():
+	# zip.get()
+	# zipLabel = Label(root, text=zip.get())
+	# zipLabel.grid(row=1, column=0, columnspan=2)
 
-	if category == "Good":
-		weather_color = "#0C0"
-	elif category == "Moderate":
-		weather_color = "#FFFF00"
-	elif category == "Unhealthy for Sensitive Groups":
-		weather_color = "#ff9900"
-	elif category == "Unhealthy":
-		weather_color = "#FF0000"
-	elif category == "Very Unhealthy":
-		weather_color = "#990066"
-	elif category == "Hazardous":
-		weather_color = "#660000"
+	try:
+		api_request = requests.get("http://www.airnowapi.org/aq/observation/zipCode/current/?format=application/json&zipCode="+ zip.get() +"&distance=5&API_KEY=0E466704-C530-4042-A2B3-EFA2C002C829")
+		api = json.loads(api_request.content)
+		city = api[0]['ReportingArea']
+		quality = api[0]['AQI']
+		category = api[0]['Category']['Name']
 
-	myLabel = Label(root, text=city + " Air Quality " + str(quality) + " " + category, font=("Helvectica"), background=weather_color)
-	myLabel.pack()
-	root.configure(background=weather_color)
+		if category == "Good":
+			weather_color = "#0C0"
+		elif category == "Moderate":
+			weather_color = "#FFFF00"
+		elif category == "Unhealthy for Sensitive Groups":
+			weather_color = "#ff9900"
+		elif category == "Unhealthy":
+			weather_color = "#FF0000"
+		elif category == "Very Unhealthy":
+			weather_color = "#990066"
+		elif category == "Hazardous":
+			weather_color = "#660000"
 
-except Exception as e:
-	api = "Error..."
+		myLabel = Label(root, text=city + " Air Quality " + str(quality) + " " + category, font=("Helvectica"), background=weather_color)
+		myLabel.grid(row=1, column=0, columnspan=2)
+		root.configure(background=weather_color)
+
+	except Exception as e:
+		api = "Error..."
+
+
+zip = Entry(root)
+zip.grid(row=0, column=0, stick=W+E+N+S)
+
+zipButton = Button(root, text="Lookup Zipcode", command=zipLookup)
+zipButton.grid(row=0, column=1, stick=W+E+N+S)
 
 root.mainloop()
